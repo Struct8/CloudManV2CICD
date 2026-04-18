e37016ab6e188b609b889bdd84b7065db744f4a1
CloudManPro <cloudmanpro111@gmail.com>
2026-04-18 14:42:11 -0300

    Update main.tf

--- 952133486861/Pipe/dev/AppCloudManV2-dev/main.tf
+++ 952133486861/Pipe/dev/AppCloudManV2-dev/main.tf
@@ -365,6 +365,9 @@ resource "aws_acm_certificate" "AppCloudManV2-dev" {
   options {
     certificate_transparency_logging_preference = "ENABLED"
   }
+  lifecycle {
+    create_before_destroy = true
+  }
   tags                              = {
     "Name" = "AppCloudManV2-dev"
     "State" = "AppCloudManV2-dev"
